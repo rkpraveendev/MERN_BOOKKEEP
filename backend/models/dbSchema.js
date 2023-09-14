6f2c97d57113f59cb9adf0182b680bcd9f222147
kumar <kumarcomputerscience1995@gmail.com>
2023-09-14 18:28:13 +0530

    db schema created

--- backend/models/dbSchema.js
+++ backend/models/dbSchema.js
@@ -2,4 +2,29 @@ import mongoose from "mongoose";
 
 // Defining a Mongoose schema for the User model.
 
- 
+const BookSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: [true, "Please provide the username"],
+        },
+        author:{
+            type: String,
+            require: [true, "Please provide the author"],
+        },
+        publishYear:{
+            type: String,
+            require: [true, "Please provide the publish year"],
+        },
+    
+    },
+    {
+    timestamp: true,
+    }
+
+)
+
+// Defining the User model based on the UserSchema.
+const Book = mongoose.models.books || mongoose.model('books', BookSchema);
+
+export default Book;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,100 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import { connectdb } from "./dbConnect.js";
+import Book from "./models/dbSchema.js";
 
 dotenv.config();
 
 const app = express();
 
-connectdb();
- 
+app.use(express.json()); // Use express.json() for JSON parsing
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  return res.status(200).send("Mern stack app");
+  res.status(200).send("Mern stack app");
 });
+
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      return res.status(400).json({
+        message: "Please provide all fields: title, author, and publishYear",
+      });
+    }
+
+    const newBook = new Book({
+      title: title,
+      author: author,
+      publishYear: publishYear,
+    });
+
+    const savedBook = await newBook.save();
+
+    res.status(200).send(savedBook);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const foundBooks = await Book.find();
+
+    res.status(200).send({
+      count: foundBooks.length,
+      data: foundBooks,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const foundBook = await Book.findById(req.params.id);
+    res.status(200).send({ foundBook });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      res.status(400).send({
+        message: "Please provide all fields: title, author, and publishYear",
+      });
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!updatedBook) {
+      return res.status(404).send("Book not found");
+    }
+
+    res.status(200).send({ message: "Book updated successfully", updatedBook });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+connectdb();
 
 const PORT = process.env.PORT || 3000; // Set a default port if PORT is not defined
 
